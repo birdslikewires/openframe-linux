43926c2da64245801ba7b4f96741437ae8272050
Andrew Davison <andydvsn@users.noreply.github.com>
2021-10-28 00:28:10 +0100

    Update of-builder.sh

--- of-builder.sh
+++ of-builder.sh
@@ -48,8 +48,8 @@ if [[ ! -d "$THISSCRIPTPATH/../$GITREPOKER" ]]; then
 	echo "`date  +'%Y-%m-%d %H:%M:%S'`: You're going to need $GITREPOURL/$GITREPOKER as well. Cloning..."
 	git clone "$GITREPOURL/$GITREPOKER" "$THISSCRIPTPATH/../$GITREPOKER"
 else
-	[[ "$USER" != "$GITKERNELOWNER" ]] && KSTSH=$(sudo -u $GITKERNELOWNER git -C "$THISSCRIPTPATH/../$GITREPOKER" stash) || KSTSH=$(git -C "$THISSCRIPTPATH/../$GITREPOKER" stash)
-	[[ "$USER" != "$GITKERNELOWNER" ]] && KPULL=$(sudo -u $GITKERNELOWNER git -C "$THISSCRIPTPATH/../$GITREPOKER" pull) || KPULL=$(git -C "$THISSCRIPTPATH/../$GITREPOKER" pull)
+	[[ "$USER" != "$GITKERNELOWNER" ]] && KSTSH=$(sudo -u $GITKERNELOWNER -H git -C "$THISSCRIPTPATH/../$GITREPOKER" stash) || KSTSH=$(git -C "$THISSCRIPTPATH/../$GITREPOKER" stash)
+	[[ "$USER" != "$GITKERNELOWNER" ]] && KPULL=$(sudo -u $GITKERNELOWNER -H git -C "$THISSCRIPTPATH/../$GITREPOKER" pull) || KPULL=$(git -C "$THISSCRIPTPATH/../$GITREPOKER" pull)
 	if [[ "$KPULL" == "Already up to date." ]]; then
 		echo "`date  +'%Y-%m-%d %H:%M:%S'`: Local copy of repository '$GITREPOKER' is up to date."
 	elif [[ "$KPULL" =~ "error: " ]] || [[ "$KPULL" =~ "fatal: " ]]; then
