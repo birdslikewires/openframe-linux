532c849883781b3ed0796fa3e8488f668806de71
Andrew Davison <andydvsn@users.noreply.github.com>
2023-06-14 23:45:04 +0100

    Update 00-of-prep.sh

--- overlay-debian-bookworm/run_on_build/00-of-prep.sh
+++ overlay-debian-bookworm/run_on_build/00-of-prep.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# 00-of-prep.sh v1.12 (14th June 2023)
+# 00-of-prep.sh v1.13 (14th June 2023)
 #  Set up the basics.
 
 #set -x
 
-DLSERVER="https://birdslikewires.net/download"
 
 OPENFRAMEUSER="of"
 
@@ -21,7 +20,7 @@ apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
 
 # Additions
-APT_SYSTEM="acpi bash-completion bc bluez ca-certificates curl dosfstools e2fsck-static e2fsprogs htop i2c-tools initramfs-tools locales libbsd0 libdaemon0 libedit2 libio-socket-ssl-perl liblockfile-bin liblockfile1 libnet-ssleay-perl libpango1.0-0 libwrap0 libx11-6 libx11-data libxau6 libxcb1 libxdmcp6 libxext6 libxmuu1 lockfile-progs lsb-release nano net-tools netplan.io ntpdate openssl patch pciutils plymouth policykit-1 psmisc rsync sudo systemd-resolved systemd-timesyncd tcpd usbutils usb-modeswitch usb-modeswitch-data unzip uuid wget wpasupplicant wireless-tools x11-xserver-utils xauth xinput yq"
+APT_SYSTEM="acpi bash-completion bc bluez ca-certificates curl dosfstools e2fsck-static e2fsprogs htop i2c-tools initramfs-tools locales libbsd0 libdaemon0 libedit2 libio-socket-ssl-perl liblockfile-bin liblockfile1 libnet-ssleay-perl libpango1.0-0 libwrap0 libx11-6 libx11-data libxau6 libxcb1 libxdmcp6 libxext6 libxmuu1 lockfile-progs lsb-release nano net-tools netplan.io ntpdate openssl patch pciutils plymouth policykit-1 psmisc rsync sudo systemd-resolved systemd-timesyncd tcpd usbutils usb-modeswitch usb-modeswitch-data unzip uuid wget wpasupplicant wireless-tools x11-xserver-utils xauth xinput yq zstd"
 APT_AUDIO="alsa-utils libmad0 libvorbisidec1 libsoxr0 mpg123"
 APT_SSH="ssh openssh-server"
 
@@ -87,14 +86,6 @@ echo
 #rm -v /etc/update-motd.d/*
 #echo
 
-# We use this for simple command line control of yaml files, eg. the netplan config file.
-# echo "Installing yq..."
-# mkdir -p /tmp/installing/yq
-# curl -k -o /tmp/installing/yq/yq $DLSERVER/openframe/deps/yq_linux_386
-# cp /tmp/installing/yq/yq /usr/local/bin/
-# rm -rf /tmp/installing/yq
-# echo
-
 echo "Ensure correct permissions..."
 [[ "$OPENFRAMEUSER" != "root" ]] && chown -R $OPENFRAMEUSER:$OPENFRAMEUSER $HOMEPATH $HOMEPATH/.*
 chown root:root /home
@@ -141,25 +132,5 @@ source /etc/os-release
 echo "Installing kernel $KERNVER into ${VERSION_CODENAME^} chroot..."
 dpkg -i /mnt/linux-image*.deb
 echo
-
-# KERNELURL="$DLSERVER/openframe/kernel/$KERNMAJVER.$KERNMIDVER/$KERNVER"
-# echo "Checking $KERNELURL for companion modules..."
-# if curl -k -f "$KERNELURL/modules-$KERNVER.tgz" >/dev/null 2>&1; then
-# 	echo
-# 	echo "Found additional modules on $DLSERVER for $KERNVER. Downloading and installing."
-# 	echo
-# 	curl -k -o /modules-$KERNVER.tgz "$KERNELURL/modules-$KERNVER.tgz"
-# 	if [ -f /modules-$KERNVER.tgz ]; then
-# 		tar zxvf /modules-$KERNVER.tgz -C /
-# 		rm /modules-$KERNVER.tgz
-# 	fi
-# else
-# 	echo
-# 	echo "No additional modules found."
-# 	echo
-# fi
-#
-# depmod -a $KERNVER
-# echo
 
 exit 0
