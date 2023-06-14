a3fc1c61f86fb427b24e0449c26bf63a9c6ba0b9
Andrew Davison <andydvsn@users.noreply.github.com>
2023-06-14 10:08:08 +0100

    Duplicate for bookworm
    Trying to remember what on earth I'm doing 😅

---
+++ overlay-debian-bookworm/run_on_build/00-of-prep.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+# 00-of-prep.sh v1.11 (6th September 2022)
+#  Set up the basics.
+
+#set -x
+
+DLSERVER="https://birdslikewires.net/download"
+
+OPENFRAMEUSER="of"
+
+
+### Packages
+
+echo
+echo "=== Packages ========================================"
+echo
+sleep 2
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+
+# Additions
+APT_SYSTEM="acpi bash-completion bc bluez ca-certificates curl dosfstools e2fsck-static e2fsprogs htop i2c-tools initramfs-tools locales libbsd0 libdaemon0 libedit2 libio-socket-ssl-perl liblockfile-bin liblockfile1 libnet-ssleay-perl libpango1.0-0 libwrap0 libx11-6 libx11-data libxau6 libxcb1 libxdmcp6 libxext6 libxmuu1 lockfile-progs lsb-release nano net-tools netplan.io ntpdate openssl patch pciutils plymouth policykit-1 psmisc rsync sudo systemd-timesyncd tcpd usbutils usb-modeswitch usb-modeswitch-data unzip uuid wget wpasupplicant wireless-tools x11-xserver-utils xauth xinput"
+APT_AUDIO="alsa-utils libmad0 libvorbisidec1 libsoxr0 mpg123"
+APT_SSH="ssh openssh-server"
+
+# Removals
+APT_REM_SYSTEM=""
+
+# Make Changes
+[[ "$APT_REM_SYSTEM" != "" ]] && apt-get remove -y --purge $APT_REM_SYSTEM
+echo
+apt-get autoremove -y
+echo
+apt-get install -y $APT_SYSTEM $APT_AUDIO $APT_SSH
+
+
+### Tweaks and Permissions
+
+echo
+echo "=== Tweaks and Permissions =========================="
+echo
+sleep 2
+
+echo "Generating en_GB.UTF-8 locale..."
+echo
+sed -i 's/^# *\(en_GB.UTF-8\)/\1/' /etc/locale.gen
+locale-gen en_GB.UTF-8
+mv /etc/localtime /etc/localtime.dist
+ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
+LANG=en_GB.UTF-8
+LC_MESSAGES=POSIX
+echo
+
+if [ "$OPENFRAMEUSER" != "root" ]; then
+
+	echo "Creating '$OPENFRAMEUSER' user and setting policy..."
+	# Create user with 'joggler' as the password.
+	useradd -m -p sa0dkJX04f4tM -s /bin/bash $OPENFRAMEUSER
+	addgroup admin
+	adduser $OPENFRAMEUSER admin
+	adduser $OPENFRAMEUSER audio
+	adduser $OPENFRAMEUSER sudo
+	adduser $OPENFRAMEUSER tty
+	adduser $OPENFRAMEUSER users
+	adduser $OPENFRAMEUSER video
+	HOMEPATH="/home/$OPENFRAMEUSER"
+	echo
+
+else
+
+	echo "root:joggler" | chpasswd
+	HOMEPATH="/root"
+
+fi
+
+echo "Setting terminal preferences..."
+# Colour terminal for all!
+for f in `find / -iname *bashrc 2>/dev/null`; do
+	sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' $f
+done
+echo
+
+# Plain old-fashioned deletions.
+#echo "Removing unnecessary components..."
+#rm -v /etc/update-motd.d/*
+#echo
+
+# We use this for simple command line control of yaml files, eg. the netplan config file.
+echo "Installing yq..."
+mkdir -p /tmp/installing/yq
+curl -k -o /tmp/installing/yq/yq $DLSERVER/openframe/deps/yq_linux_386
+cp /tmp/installing/yq/yq /usr/local/bin/
+rm -rf /tmp/installing/yq
+echo
+
+echo "Ensure correct permissions..."
+[[ "$OPENFRAMEUSER" != "root" ]] && chown -R $OPENFRAMEUSER:$OPENFRAMEUSER $HOMEPATH $HOMEPATH/.*
+chown root:root /home
+chmod +s /bin/ping /bin/ping6 /bin/su /usr/bin/sudo /usr/sbin/ntpdate
+chown -R root:root /etc/sudoers.d
+chmod -R 440 /etc/sudoers.d/*
+chown root:root /usr/local/bin/*
+chown root:root /usr/local/sbin/*
+chmod 755 /usr/local/bin/*
+chmod 755 /usr/local/sbin/*
+chmod 644 /usr/local/sbin/*.ver
+echo
+
+echo "Enable systemd services..."
+/bin/systemctl enable systemd-resolved.service
+/bin/systemctl enable systemd-timesyncd.service
+chmod +x /usr/local/sbin/of-*
+chmod +x /usr/sbin/*
+for f in `ls -1 /lib/systemd/system | grep 'of-' | grep '.service'`; do
+  SERVICE=`echo $f | awk -F\.service {'print $1'}`
+  /bin/systemctl enable $SERVICE
+  echo "Enabled $SERVICE"
+done
+echo
+
+echo "Enable password authentication for root user over SSH..."
+[[ "$OPENFRAMEUSER" == "root" ]] && echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+
+
+### Kernel Installation
+
+echo
+echo "=== Kernel Installation ============================="
+echo
+sleep 2
+
+KERNVER=`ls /mnt/ | grep linux-image | awk -F\- '{print $3}' | awk -F\_ '{print $1}'`
+KERNMAJVER=`echo $KERNVER | awk -F\. {'print $1'}`
+KERNMIDVER=`echo $KERNVER | awk -F\. {'print $2'}`
+KERNMINVER=`echo $KERNVER | awk -F\. {'print $3'}`
+
+source /etc/os-release
+
+echo "Installing kernel $KERNVER into ${VERSION_CODENAME^} chroot..."
+dpkg -i /mnt/linux-image*.deb
+echo
+
+# KERNELURL="$DLSERVER/openframe/kernel/$KERNMAJVER.$KERNMIDVER/$KERNVER"
+# echo "Checking $KERNELURL for companion modules..."
+# if curl -k -f "$KERNELURL/modules-$KERNVER.tgz" >/dev/null 2>&1; then
+# 	echo
+# 	echo "Found additional modules on $DLSERVER for $KERNVER. Downloading and installing."
+# 	echo
+# 	curl -k -o /modules-$KERNVER.tgz "$KERNELURL/modules-$KERNVER.tgz"
+# 	if [ -f /modules-$KERNVER.tgz ]; then
+# 		tar zxvf /modules-$KERNVER.tgz -C /
+# 		rm /modules-$KERNVER.tgz
+# 	fi
+# else
+# 	echo
+# 	echo "No additional modules found."
+# 	echo
+# fi
+#
+# depmod -a $KERNVER
+# echo
+
+exit 0
---
+++ overlay-debian-bookworm/run_on_build/99-of-clean.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# 99-of-clean.sh v1.24 (10th February 2022)
+#  Used to clean an OpenFrame.
+
+OPTIONS=""
+
+if [ "$USER" != "root" ]; then
+  echo "Must be run as root."
+  exit
+fi
+
+# Get the kernel version.
+CHROOTKERN=`ls /mnt/ | grep linux-image`
+if [ "$CHROOTKERN" != "" ]; then
+  KERNEL=`ls /mnt/ | grep linux-image | awk -F\- '{print $3}' | awk -F\_ '{print $1}'`
+else
+  KERNEL=`uname -r`
+fi
+
+echo
+echo "=== Wind Down ======================================="
+echo
+sleep 2
+
+# Disable unwanted services.
+/bin/systemctl disable e2scrub_reap.service
+
+# This bit removes the development packages, source code and man files.
+if [[ "$@" == "dev" ]] || [[ "$OPTIONS" =~ "dev" ]]; then
+  echo
+  if [ -d /lib/modules/$KERNEL/updates/dkms ]; then
+    echo "Backing up DKMS kernel modules..."
+    mv /lib/modules/$KERNEL/updates/dkms /lib/modules/$KERNEL/updates/dkms.bak
+  fi
+  echo "Removing development packages and source code..."
+  apt-get -y remove build-essential dkms eject fakeroot gcc libc-dev-bin libc6-dev libgomp1 libquadmath0 libxft2 linux-headers-$KERNEL linux-libc-dev make manpages manpages-dev rt2870sta-dkms 2>/dev/null
+  echo
+  if [ -d /lib/modules/$KERNEL/updates/dkms ]; then
+    echo "Restoring previously installed DKMS kernel modules..."
+    mv /lib/modules/$KERNEL/updates/dkms.bak /lib/modules/$KERNEL/updates/dkms
+  fi
+  echo "Removing man files and example code..."
+  rm -rf /usr/src /usr/share/doc /usr/share/man /usr/share/doc-base /usr/share/sounds/alsa/* /var/lib/dkms/*
+  depmod -a $KERNEL
+fi
+
+if [[ "$@" == "ssh" ]] || [[ "$OPTIONS" =~ "ssh" ]]; then
+  echo
+  echo "Removing SSH server keys..."
+  rm -v /etc/ssh/ssh_host*
+fi
+
+echo
+echo "Tidying the package manager..."
+apt-get -y autoremove
+apt-get -y clean
+echo
+
+echo "Deleting caches..."
+rm -rvf /opt/squeezeplay/bin/gmon.out
+rm -rvf /temp
+rm -rf /var/cache/apt/*.bin
+rm -rf /var/lib/apt/lists
+mkdir -p /var/lib/apt/lists/partial
+echo
+
+echo "Cleaning /etc..."
+rm -rvf /etc/resolv.conf
+rm -rvf /etc/sqpbeta
+
+echo "Relinking systemd resolved..."
+ln -sv /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+echo
+echo "Sweeping out the root account..."
+rm -rfv /root/.aptitude
+rm -rfv /root/.bash_history
+rm -rfv /root/.cache
+rm -rfv /root/.debtags
+rm -rfv /root/.local
+echo
+
+echo
+[[ "$CHROOTKERN" == "" ]] && df -h
+echo
+echo
+echo "Cleaning complete."
+echo
